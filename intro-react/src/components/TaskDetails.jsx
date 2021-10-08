fad4d7ae2e8d47110689a4e98eaf174cb3d5a2b7
Yasmin Benicio <79879086+yasminvl@users.noreply.github.com>
2021-10-08 11:05:36 -0300

    Add files via upload

---
+++ intro-react/src/components/TaskDetails.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+
+import Button from './Button';
+
+import './TaskDetails.css';
+
+const TaskDetails = () => {
+    const params = useParams();
+    const history = useHistory();
+
+    const handleBackButtonClick = () => {
+        history.goBack();
+    }
+    
+    return ( 
+        <>
+            <div className="back-button-container">
+                <Button onClick={handleBackButtonClick}>Voltar</Button>
+            </div>
+            <div className="task-details-container">
+				<h2>{params.taskTitle}</h2>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus inventore rem porro a maiores tempora, reiciendis velit impedit. Modi eaque culpa veritatis necessitatibus adipisci dolorem eveniet nisi ipsam vitae expedita?
+                </p>
+            </div>
+        </>
+     );
+}
+ 
+export default TaskDetails;

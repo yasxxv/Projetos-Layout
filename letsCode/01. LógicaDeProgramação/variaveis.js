5a0b422238b489afb58cbef85bb92111cc2db50f
Yasmin Benicio <79879086+yasxxv@users.noreply.github.com>
2022-05-05 17:19:35 -0300

    Create variaveis.js

---
+++ letsCode/01. LógicaDeProgramação/variaveis.js
@@ -0,0 +1,24 @@
+//console.log("Funciona!");
+
+//boolean
+var a = true;
+var b = false;
+
+//Number
+var c = 10;
+var d = 11.5;
+
+//String
+var e = "teste";
+
+//Undefined
+var f;
+var g = undefined;
+//function
+var a = function(){};
+var b = () => {};
+
+//object
+var c = {};
+var d = []; //array
+var e = null;
